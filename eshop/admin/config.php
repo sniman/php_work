0bea1cb95e53bd0e9d4fd0064334287f1e30743d
snoriman <snoriman@gmail.com>
2015-12-16 22:07:39 +0800

    add homelivingshop.com

---
+++ eshop/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/eshop/admin/');
+define('HTTP_CATALOG', 'http://localhost:8888/eshop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/eshop/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8888/eshop/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/eshop/admin/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/eshop/system/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/eshop/admin/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/eshop/admin/view/template/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/eshop/system/config/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/eshop/image/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/eshop/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/eshop/system/storage/download/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/eshop/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/eshop/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/MAMP/htdocs/eshop/system/storage/upload/');
+define('DIR_CATALOG', '/Applications/MAMP/htdocs/eshop/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'eshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
